933ea46c251cafd8cf7300c27a29e07a2fa9a7c2
iohan sardinha <ioxsardinha@hotmail.com>
2016-07-21 17:26:37 -0300

    First Commit

---
+++ Olimpiada/Olimpiada/MainActivity.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Android.Gms.Maps;
+
+namespace Olimpiada
+{
+    [Activity(Label = "Olimpiada", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity, IOnMapReadyCallback
+    {
+        private GoogleMap map;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+           
+            SetContentView(Resource.Layout.Main);
+
+            setUpMap();
+
+        }
+
+        private void setUpMap()
+        {
+            if(map == null)
+            {
+                FragmentManager.FindFragmentById<MapFragment>(Resource.Id.map).GetMapAsync(this);
+            }
+        }
+
+        public void OnMapReady(GoogleMap googleMap)
+        {
+            map = googleMap;
+        }
+    }
+}
+
